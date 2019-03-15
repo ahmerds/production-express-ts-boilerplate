cb0b6cf4ccee8ef65893d17291e6c1b8db5276b0
grey <a.shehu@ehealth4everyone.com>
2019-03-15 11:00:50 +0100

    First commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import http from 'http';
+import express from 'express';
+import middleware from './middleware';
+import { applyMiddleware } from './utils';
+
+const router = express();
+applyMiddleware(middleware, router);
+
+const { PORT = 3000 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () => 
+    console.log(`Server running on http://localhost:${PORT}`)
+);
